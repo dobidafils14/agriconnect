d126f6d794fc3ff7e18659c21857aeb28af10914
DOBIDAFILS14 <dobidafils14@gmail.com>
2026-06-16 16:43:38 +0200

    Connexion frontend vers backend Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
+const api = axios.create({
+  baseURL: "https://agriconnect-backend-d0e2.onrender.com/api"
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 

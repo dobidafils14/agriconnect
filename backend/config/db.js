a1424dc7a79994d95e36f786b2796f996db0214a
DOBIDAFILS14 <dobidafils14@gmail.com>
2026-06-16 16:18:16 +0200

    Test connexion Aiven

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Kalash@2002",
-  database: "agriconnect",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0

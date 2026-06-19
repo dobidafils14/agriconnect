e1319abacf7bebd8b22775ecf36de5e43bd7a25b
DOBIDAFILS14 <dobidafils14@gmail.com>
2026-06-19 17:44:53 +0200

    Correction affichage images produits et profil

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-
-  ssl: {
-    rejectUnauthorized: false
-  },
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Kalash@2002",
+  database: "agriconnect",
 
   waitForConnections: true,
   connectionLimit: 10,
@@ -20,7 +16,7 @@ db.getConnection((err, connection) => {
   if (err) {
     console.error("ERREUR MYSQL :", err);
   } else {
-    console.log("✅ Connexion MySQL Aiven réussie !");
+    console.log("✅ Connexion MySQL locale réussie !");
     connection.release();
   }
 });
